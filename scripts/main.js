fc44d3add7b43c33e8601c06ffa51892605523f9
rdcampos196 <126531101+rdcampos196@users.noreply.github.com>
2023-02-28 16:03:41 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let myVariable = "Bob";
+myVariable = "Steve";
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.jpg") {
+    myImage.setAttribute("src", "images/firefox2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.jpg");
+  }
+}
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.onclick = () => {
+  setUserName();
+};
